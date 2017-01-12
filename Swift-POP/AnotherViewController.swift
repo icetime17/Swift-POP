6459b355c9ce77ced802c33b5e68bbd829afb207
Chris Hu <icetime17@126.com>
2017-01-12 14:24:55 +0800

    Step 2:  将parse response的代码分离

--- Swift-POP/AnotherViewController.swift
+++ Swift-POP/AnotherViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 
+// https://api.github.com/users/onevcat
 private struct GithubUserInfo {
     let name: String
     let company: String
@@ -39,6 +40,28 @@ private struct GithubUserInfo {
 // GithubUserInfo(data: data)
 
 
+
+// http://httpbin.org/get
+private struct HttpBinInfo {
+    let origin: String
+    let url: String
+    
+    init?(data: Data) {
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else { return nil }
+        
+        guard let origin    = json?["origin"] as? String else { return nil }
+        guard let url       = json?["url"] as? String else { return nil }
+        
+        self.origin = origin
+        self.url    = url
+    }
+}
+// HttpBinInfoRequest()
+
+
+
+// MARK: - request
+
 private enum HTTPMethod: String {
     case GET    = "GET"
     case POST   = "POST"
@@ -51,9 +74,18 @@ private protocol Request {
     var method: HTTPMethod { get }
     var parameter: [String: Any] { get }
     
+    // 要针对不同的结构体或类指定不同的Response，类似泛型功能
+    associatedtype Response: Decodable
+    
+    func rquestInfo()
+    
+    func parseResponse(data: Data)
 }
 
 private struct GithubUserInfoRequest: Request {
+    // 指定在该struct中，Response即为GithubUserInfo
+    typealias Response = GithubUserInfo
+
     let name: String
     
     let host: String = "https://api.github.com"
@@ -63,20 +95,90 @@ private struct GithubUserInfoRequest: Request {
     let method: HTTPMethod = .GET
     let parameter: [String : Any] = [:]
     
-    mutating func rquestInfo() {
+    func rquestInfo() {
         let url = URL(string: "\(host)\(path)")
         var request = URLRequest(url: url!)
         request.httpMethod = method.rawValue
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let data = data {
-                let userInfo = GithubUserInfo(data: data)
-                print(userInfo)
+                self.parseResponse(data: data)
             }
         }
         task.resume()
     }
+    
+    func parseResponse(data: Data) {
+//        let userInfo = GithubUserInfo(data: data)
+//        print(userInfo)
+        
+        if let userInfo = Response.parse(data: data) {
+            print(">>>>>>")
+            print(userInfo)
+            print(">>>>>>\n")
+        }
+    }
 }
+
+
+private struct HttpBinInfoRequest: Request {
+    // 指定在该struct中，Response即为HttpBinInfoRequest
+    typealias Response = HttpBinInfo
+    
+    let host: String = "http://httpbin.org"
+    var path: String {
+        return "/get"
+    }
+    let method: HTTPMethod = .GET
+    let parameter: [String : Any] = [:]
+    
+    func rquestInfo() {
+        let url = URL(string: "\(host)\(path)")
+        var request = URLRequest(url: url!)
+        request.httpMethod = method.rawValue
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                self.parseResponse(data: data)
+            }
+        }
+        task.resume()
+    }
+    
+    func parseResponse(data: Data) {
+        //        let httpBinInfo = HttpBinInfo(data: data)
+        //        print(httpBinInfo)
+        
+        if let httpBinInfo = Response.parse(data: data) {
+            print(">>>>>>")
+            print(httpBinInfo)
+            print(">>>>>>\n")
+        }
+    }
+}
+
+
+// MARK: - parse
+
+// 如何将parse分离
+private protocol Decodable {
+    static func parse(data: Data) -> Self?
+}
+
+// 使得GithubUserInfo有了parse的能力
+extension GithubUserInfo: Decodable {
+    static func parse(data: Data) -> GithubUserInfo? {
+        return GithubUserInfo(data: data)
+    }
+}
+
+// 使得HttpBinInfo有了parse的能力
+extension HttpBinInfo: Decodable {
+    static func parse(data: Data) -> HttpBinInfo? {
+        return HttpBinInfo(data: data)
+    }
+}
+
 
 class AnotherViewController: UIViewController {
 
@@ -98,6 +200,21 @@ class AnotherViewController: UIViewController {
         var userInfoRequest = GithubUserInfoRequest(name: "onevcat")
         userInfoRequest.rquestInfo()
         
+        
+        
+        /*
+        let urlString = "http://httpbin.org/get"
+        guard let url = URL(string: urlString) else { return }
+        do {
+            guard let httpBinInfo = try HttpBinInfo(data: Data(contentsOf: url)) else { return }
+            print(httpBinInfo)
+        } catch {
+            print("error")
+        }
+        */
+        
+        var httpBinRequest = HttpBinInfoRequest()
+        httpBinRequest.rquestInfo()
         
     }
     
